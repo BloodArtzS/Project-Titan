d18999066ac85a85cea1137a92a3431be2be2395
BloodArtzS <bloodartzstudioz@gmail.com>
2025-02-01 11:47:51 -0500

    inspo start

--- src/app/inspo/inspo.component.ts
+++ src/app/inspo/inspo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-inspo',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './inspo.component.sass'
 })
 export class InspoComponent {
+  constructor(private titleService: Title) {}
 
+  ngOnInit() {
+    this.titleService.setTitle('Inspiration Page');
+  }
 }
