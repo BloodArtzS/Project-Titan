6a15acac2dd049b1c4b1d3b0f93a154eeb19bf25
BloodArtzS <bloodartzstudioz@gmail.com>
2025-05-15 09:37:58 +0530

    Chapters Start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { TitanComponent } from './titan/titan.component';
 import { InspoComponent } from './inspo/inspo.component';
 import { ContactComponent } from './contact/contact.component';
+import { Chapter1Component } from './titan/chapters/chapter1/chapter1.component';
 
 export const routes: Routes = [
  { path: '', component: WelcomeComponent},
  { path: 'titan', component: TitanComponent},
  { path: 'inspo', component: InspoComponent},
- { path: 'contact', component: ContactComponent}
+ { path: 'contact', component: ContactComponent},
+ { path: 'titan/chapter1', component: Chapter1Component}
 ];
 
 @NgModule({
