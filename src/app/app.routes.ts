b78c5d8d7124d1ad16a4cd956d46414385c6885e
BloodArtzS <bloodartzstudioz@gmail.com>
2025-10-01 05:08:22 -0400

    Added 2 new pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,9 @@ import {NgModule} from "@angular/core";
 import { WelcomeComponent } from '../welcome/welcome.component';
 import { InspoComponent } from '../inspo/inspo.component';
 import { ContactComponent } from '../contact/contact.component';
-import { Chapter1Component } from '../chapters/chapter1/chapter1.component';
 import { ChapterComponent } from '../chapters/chapter.component';
+import { ResourcesComponent } from '../resources/resources.component';
+import { DictionaryComponent } from '../dictionary/dictionary.component';
 
 
 export const routes: Routes = [
@@ -12,6 +13,8 @@ export const routes: Routes = [
  { path: 'chapters', component: ChapterComponent},
  { path: 'inspo', component: InspoComponent},
  { path: 'contact', component: ContactComponent},
+ { path: 'dictionary', component: DictionaryComponent},
+ { path: 'resources', component: ResourcesComponent},
  { path: 'chapter1', loadComponent: () => import('../chapters/chapter1/chapter1.component').then(m => m.Chapter1Component) },
  { path: 'chapter2', loadComponent: () => import('../chapters/chapter2/chapter2.component').then(m => m.Chapter2Component) },
  { path: 'chapter3', loadComponent: () => import('../chapters/chapter3/chapter3.component').then(m => m.Chapter3Component) },
