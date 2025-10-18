d3c56f12bf3c65a9563e37003cd8fa90b8d34442
BloodArtzS <bloodartzstudioz@gmail.com>
2025-10-18 08:56:08 -0400

    Ready for release

--- src/chapters/chapter1/chapter1.component.ts
+++ src/chapters/chapter1/chapter1.component.ts
@@ -8,19 +8,36 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./chapter1.component.sass', '../chapter.component.sass'],
 })
 export class Chapter1Component {
+  views: number = 0;
+
+  chapters = [
+    { title: 'Chapter 1: Awake from the Slumber', route: '/chapter1' },
+    { title: 'Chapter 2: The Protocol', route: '/chapter2' },
+    { title: 'Chapter 3: Shadows', route: '/chapter3' },
+    { title: 'Chapter 4: Broken Alliance', route: '/chapter4' },
+  ];
+
   constructor(private titleService: Title) {}
 
   ngOnInit() {
     this.titleService.setTitle('Chapter 1 - Project Titan');
+    if (typeof window !== 'undefined') {
+      this.updateViewCount();
+    }
   }
 
-  // parent.component.ts
-chapters = [
-  { title: 'Chapter 1: Awake from the Slumber', route: '/chapter1' },
-  { title: 'Chapter 2: The Protocol', route: '/chapter2' },
-  { title: 'Chapter 3: Shadows', route: '/chapter3' },
-  { title: 'Chapter 4: Broken Alliance', route: '/chapter4' },
-];
+  async updateViewCount() {
+    const namespace = 'bloodartz.netlify.app';
+    const key = 'chapter1';
+    const url = `https://api.countapi.xyz/hit/${namespace}/${key}`;
 
-
+    try {
+      const res = await fetch(url);
+      if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+      const data = await res.json();
+      this.views = data.value;
+    } catch (err) {
+      console.error('Error fetching views:', err);
+    }
+  }
 }
