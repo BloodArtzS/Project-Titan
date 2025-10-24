7888c1c8e88e8196a2f5fbb0708bde16a300109b
BloodArtzS <bloodartzstudioz@gmail.com>
2025-10-24 09:33:51 -0400

    Added Up arrow button
    Added up arrow button to chapter specific pages

--- src/chapters/chapter1/chapter1.component.ts
+++ src/chapters/chapter1/chapter1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -8,7 +8,6 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./chapter1.component.sass', '../chapter.component.sass'],
 })
 export class Chapter1Component {
-  views: number = 0;
 
   chapters = [
     { title: 'Chapter 1: Awake from the Slumber', route: '/chapter1' },
@@ -21,23 +20,83 @@ export class Chapter1Component {
 
   ngOnInit() {
     this.titleService.setTitle('Chapter 1 - Awake from the Slumber');
-    if (typeof window !== 'undefined') {
-      this.updateViewCount();
-    }
   }
 
-  async updateViewCount() {
-    const namespace = 'bloodartz-netlify-app';
-    const key = 'trw-chapter1';
-    const url = `https://api.countapi.xyz/update/${namespace}/${key}/?amount=1`;
+  showScrollButton = false;
+  private lastScrollY = 0;
 
-    try {
-      const res = await fetch(url);
-      if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
-      const data = await res.json();
-      this.views = data.value;
-    } catch (err) {
-      console.error('Error fetching views:', err);
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScroll = window.scrollY;
+
+    // Only show button if user scrolls UP and not at top
+    if (currentScroll < this.lastScrollY && currentScroll > 200) {
+      this.showScrollButton = true;
+    } else {
+      this.showScrollButton = false;
     }
+
+    this.lastScrollY = currentScroll;
   }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  // ---------------------
+  // YOUTUBE SEGMENT LOOP
+  // ---------------------
+  // private player: any;
+  // private startTime = 8;
+  // private endTime = 20;
+  // private loopCount = 0;
+  // private maxLoops = 3;
+
+  // ngAfterViewInit(): void {
+  //   // Load YouTube IFrame API dynamically
+  //   const tag = document.createElement('script');
+  //   tag.src = 'https://www.youtube.com/iframe_api';
+  //   document.body.appendChild(tag);
+
+  //   (window as any).onYouTubeIframeAPIReady = () => {
+  //     this.player = new (window as any).YT.Player('yt-segment', {
+  //       videoId: 'QNUJDRYtfZ4',
+  //       playerVars: {
+  //         start: this.startTime,
+  //         controls: 0,
+  //         modestbranding: 1,
+  //         rel: 0,
+  //       },
+  //       events: {
+  //         onReady: this.onPlayerReady.bind(this),
+  //         onStateChange: this.onPlayerStateChange.bind(this),
+  //       },
+  //     });
+  //   };
+  // }
+
+  // private onPlayerReady(event: any) {
+  //   event.target.seekTo(this.startTime);
+  //   event.target.playVideo();
+  // }
+
+  // private onPlayerStateChange(event: any) {
+  //   if (event.data === (window as any).YT.PlayerState.PLAYING) {
+  //     const check = setInterval(() => {
+  //       const current = this.player.getCurrentTime();
+  //       if (current >= this.endTime) {
+  //         clearInterval(check);
+  //         this.loopCount++;
+  //         if (this.loopCount < this.maxLoops) {
+  //           this.player.seekTo(this.startTime);
+  //         } else {
+  //           this.player.pauseVideo();
+  //         }
+  //       }
+  //     }, 500);
+  //   }
+  // }
+
+  
+
 }
